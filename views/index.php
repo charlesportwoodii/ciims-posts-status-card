600fe134bdfc2d3d1128dce8497919850f05f225
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-08-19 21:12:19 -0500

    Removing Highcharts js

--- views/index.php
+++ views/index.php
@@ -1,6 +1,4 @@
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl.$asset; ?>/css/card.css" />
-<script type="text/javascript" src="<?php echo Yii::app()->baseUrl.$asset; ?>/Highcharts.js/js/highcharts.js"></script>
-<script type="text/javascript" src="<?php echo Yii::app()->baseUrl.$asset; ?>/Highcharts.js/js/highcharts-more.js"></script>
 
 <div id="dASaghwDa" data-attr-id="<?php echo $model->id; ?>">
 	<div class="card-header">
